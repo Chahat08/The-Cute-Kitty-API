efd9405593d5975d7d74c889a32f569e07f77092
Chahat08 <chahat.ck88@gmail.com>
2022-10-18 09:36:16 +0530

    kitty model created!

--- catto/models.py
+++ catto/models.py
@@ -1,3 +1,13 @@
+import random
 from django.db import models
 
 # Create your models here.
+
+class Kitty(models.Model):
+    name = models.CharField(max_length=100, blank=True, default=str(random.randint(1,1000)))
+    img = models.ImageField(upload_to="cats/", blank=True)
+    breed = models.CharField(max_length=100, blank=True, default="cute")
+
+    def __str__(self):
+        return self.name
+
