80850cf1c5fd4840636d099b3f7f36aed3e3b8c2
Chahat08 <chahat.ck88@gmail.com>
2022-10-19 11:39:29 +0530

    cutekitty api v1 ready!!!

--- catto/urls.py
+++ catto/urls.py
@@ -1,7 +1,9 @@
 # catto/urls.py
 from django.urls import path
-from .views import KittyList
+from .views import KittyList, KittyCat, PickCatto
 
 urlpatterns = [
     path('', KittyList.as_view(), name='kitties'),
+    path('catto/<int:pk>/', KittyCat.as_view(), name='catto'),
+    path('catto/random/', PickCatto)
 ]
--- catto/views.py
+++ catto/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.urls import reverse
 from rest_framework import generics
 from . import serializers, models
+from django.shortcuts import redirect
+import random
 
 # Create your views here.
 class KittyList(generics.ListAPIView): 
     serializer_class = serializers.KittySerializer
     queryset = models.Kitty.objects.all()
+
+
+class KittyCat(generics.RetrieveAPIView):
+    serializer_class = serializers.KittySerializer
+    queryset = models.Kitty.objects.all()
+
+
+def PickCatto(request):
+    pks =  models.Kitty.objects.values_list('pk', flat=True)
+    pk = random.choice(pks)
+    return redirect(reverse('kitties')+r'catto/{}/'.format(str(pk)))
