177772df7d53e622825d63e843a9c94d7da599cc
Chahat08 <chahat.ck88@gmail.com>
2022-10-19 23:25:36 +0530

    cat searching and filtering implemented

--- catto/urls.py
+++ catto/urls.py
@@ -1,9 +1,10 @@
 # catto/urls.py
 from django.urls import path
-from .views import KittyList, KittyCat, PickCatto
+from .views import KittyList, KittyCat, PickCatto, SearchKittyCat
 
 urlpatterns = [
     path('', KittyList.as_view(), name='kitties'),
     path('catto/<int:pk>/', KittyCat.as_view(), name='catto'),
-    path('catto/random/', PickCatto)
+    path('catto/random/', PickCatto),
+    path('catto/search/', SearchKittyCat.as_view(), name='search')
 ]
--- catto/views.py
+++ catto/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics
 from . import serializers, models
 from django.shortcuts import redirect
 import random
+from django.db.models import Q
+from itertools import chain
 
 # Create your views here.
 class KittyList(generics.ListAPIView): 
@@ -13,6 +15,26 @@ class KittyList(generics.ListAPIView):
 class KittyCat(generics.RetrieveAPIView):
     serializer_class = serializers.KittySerializer
     queryset = models.Kitty.objects.all()
+
+
+class SearchKittyCat(generics.ListAPIView):
+    serializer_class = serializers.KittySerializer
+    
+    def get_queryset(self):
+        query = self.request.GET
+
+        name_list = []
+        breed_list = []
+
+        if 'name' in query.keys():
+            name_list = models.Kitty.objects.filter(name__icontains=query['name'])
+
+        if 'breed' in query.keys():
+            breed_list = models.Kitty.objects.filter(breed__icontains=query['breed'])
+
+
+        return list(chain(name_list, breed_list))
+        
 
 
 def PickCatto(request):
