06862b2e535935b92f78f3b188927686e2fa00f6
Chahat08 <chahat.ck88@gmail.com>
2022-10-21 19:01:52 +0530

    deployment host added

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-96#-=9*a1m1lzd$cbmvtqnbcac^7erxl8jy_o&a71(j7rokinb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000', 'http://cutekitty.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'cutekitty.pythonanywhere.com']
 
 
 # Application definition
